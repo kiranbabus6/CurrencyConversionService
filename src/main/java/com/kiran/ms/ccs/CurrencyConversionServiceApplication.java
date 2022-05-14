2285998c2596dcc8465ef5cb0c233340965eda84
Kiran <ksekhara3@gatech.edu>
2022-05-15 01:50:23 +0530

    feign

--- src/main/java/com/kiran/ms/ccs/CurrencyConversionServiceApplication.java
+++ src/main/java/com/kiran/ms/ccs/CurrencyConversionServiceApplication.java
@@ -3,6 +3,10 @@ package com.kiran.ms.ccs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+/**
+ * @author kiran
+ *
+ */
 @EnableFeignClients
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
